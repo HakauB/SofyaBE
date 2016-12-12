090cc7fffbcb7d973adb8e8b94a458095bb4d60c
HakauB <ballardhakau@gmail.com>
2016-12-13 00:26:35 +1300

    Refactored down

--- fileviewer/views.py
+++ fileviewer/views.py
@@ -12,6 +12,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 class JSONResponse(HttpResponse):
     """
@@ -37,46 +39,11 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-class FileHouseList(APIView):
-    """
-    List all filehouses, or create a new filehouse.
-    """
-    def get(self, request, format=None):
-        filehouses = FileHouse.objects.all()
-        serializer = FileHouseSerializer(filehouses, many=True)
-        return Response(serializer.data)
+class FileHouseList(generics.ListCreateAPIView):
+    queryset = FileHouse.objects.all()
+    serializer_class = FileHouseSerializer
 
-    def post(self, request, format=None):
-        serializer = FileHouseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class FileHouseDetail(APIView):
-    """
-    Retrieve, update or delete a filehouse instance.
-    """
-    def get_object(self, pk):
-        try:
-            return FileHouse.objects.get(pk=pk)
-        except FileHouse.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        filehouse = self.get_object(pk)
-        serializer = FileHouseSerializer(filehouse)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        filehouse = self.get_object(pk)
-        serializer = FileHouseSerializer(filehouse, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        filehouse = self.get_object(pk)
-        filehouse.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class FileHouseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FileHouse.objects.all()
+    serializer_class = FileHouseSerializer
