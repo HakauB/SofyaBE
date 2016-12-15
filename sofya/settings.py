dfd551c658943d0469bd7d421bbd249ec7f573d0
HakauB <ballardhakau@gmail.com>
2016-12-15 21:59:39 +1300

    Fixed some config

--- sofya/settings.py
+++ sofya/settings.py
@@ -139,6 +139,7 @@ REST_FRAMEWORK = {
 #    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
 #}
 
+TRANSPORT_URL = 'redis://localhost:6379'
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
